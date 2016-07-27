a26b7cd858269cd17085ba07cb898b5fbfc7b12a
Nikolas Kopp <nikolas.kopp@erdfisch.de>
2016-07-27 14:49:51 +0200

    readd gulp file

---
+++ app/gulp.config.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var watch = require('gulp-watch');
+
+gulp.task('build', function () {
+  var processors = [
+    require('precss')(),
+    require('postcss-cssnext')(),
+    require('lost')
+  ];
+  return gulp.src('./client/theme.css')
+  .pipe(postcss(processors))
+  .pipe(gulp.dest('../dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./client/**/*.css', ['build']);
+});
