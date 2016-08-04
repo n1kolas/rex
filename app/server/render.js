9baf9d2d1db89bf9593bd7150ba6f3c785f14669
Nikolas Kopp <nikolas.kopp@erdfisch.de>
2016-08-04 10:26:47 +0200

    fix some paths

--- app/server/render.js
+++ app/server/render.js
@@ -8,7 +8,7 @@ import getRoutes from '../client/routes/routes.js';
 
 const app = express();
 
-const distPath = path.join(__dirname, '../../dist');
+const distPath = path.join(__dirname, '../../public');
 
 app.use(express.static(distPath, {index: false}));
 
@@ -34,12 +34,12 @@ function renderPage(markup) {
     <html>
       <head>
         <meta charset="UTF-8" />
-        <link rel="stylesheet" href="/theme.css" type="text/css" />
+        <link rel="stylesheet" href="/css/theme.css" type="text/css" />
         <title>rex - an opinionated react starterkit.</title>
       </head>
       <body>
         <div id="app">${markup}</div>
-        <script src="/bundle.js"></script>
+        <script src="/js/bundle.js"></script>
       </body>
     </html>
    `
